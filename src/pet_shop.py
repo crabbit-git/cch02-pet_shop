1e7023b58ca3795714b46448afd4f6b34f9c29b3
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-17 18:30:35 +0000

    All tests pass

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -57,8 +57,9 @@ def customer_can_afford_pet(customer, pet):
         return False
 
 def sell_pet_to_customer(pet_shop, pet, customer):
-    remove_customer_cash(customer, pet["price"])
-    add_or_remove_cash(pet_shop, pet["price"])
-    remove_pet_by_name(pet_shop, pet["name"])
-    increase_pets_sold(pet_shop, 1)
-    add_pet_to_customer(customer, pet)
+    if pet in pet_shop["pets"] and customer["cash"] >= pet["price"]:
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        remove_pet_by_name(pet_shop, pet["name"])
+        increase_pets_sold(pet_shop, 1)
+        add_pet_to_customer(customer, pet)
