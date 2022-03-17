051e6ae8b21e86c331bb91455ac8c3af255139f7
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-17 16:28:14 +0000

    First integration test passes

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -56,3 +56,9 @@ def customer_can_afford_pet(customer, pet):
     else:
         return False
 
+def sell_pet_to_customer(pet_shop, pet, customer):
+    remove_customer_cash(customer, pet["price"])
+    add_or_remove_cash(pet_shop, pet["price"])
+    remove_pet_by_name(pet_shop, pet["name"])
+    increase_pets_sold(pet_shop, 1)
+    add_pet_to_customer(customer, pet)
